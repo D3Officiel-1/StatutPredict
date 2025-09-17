75ec9f488e605b8d62fe032702da17f06d613fb0
Desire Koua <desire0369@gmail.com>
2025-09-17 00:10:27 +0000

    fait en sorte que l'app utilise le mode sombre par défaut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr" suppressHydrationWarning>
+    <html lang="fr" className="dark" suppressHydrationWarning>
        <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
