6b80dbeae38a98449fc119a5ac003cd78259d0bb
Desire Koua <desire0369@gmail.com>
2025-10-02 02:31:49 +0000

    fait en sorte que l'animation de chargement SVG ne soit pas en rotation

--- src/components/ui/custom-loader.tsx
+++ src/components/ui/custom-loader.tsx
@@ -10,7 +10,7 @@ export default function CustomLoader({ className }: CustomLoaderProps) {
       <img
         src="https://1play.gamedev-tech.cc/lucky_grm/assets/media/c544881eb170e73349e4c92d1706a96c.svg"
         alt="Chargement..."
-        className="animate-spin h-6 w-6"
+        className="h-6 w-6"
       />
     </div>
   );
