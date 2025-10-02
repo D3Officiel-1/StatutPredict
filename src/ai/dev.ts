403580cc82d8da3116dab2d7fa52ffcd1bb68989
Desire Koua <desire0369@gmail.com>
2025-10-02 08:15:28 +0000

    supprime tout le code qui concerne brevo

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -4,4 +4,3 @@ config();
 import '@/ai/flows/intelligent-notification-suggestions.ts';
 import '@/ai/flows/maintenance-message-generator.ts';
 import '@/ai/flows/generate-discount-image.ts';
-import '@/ai/flows/send-email-flow.ts';
