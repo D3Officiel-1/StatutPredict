0f0a9acaa56420fe7c5bb68f1dabcd519bf7221b
Desire Koua <desire0369@gmail.com>
2025-10-02 02:45:38 +0000

    fait en sorte que la barre latérale du panneau administrateur soit parei

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -62,7 +62,7 @@ export default {
           '5': 'hsl(var(--chart-5))',
         },
         sidebar: {
-          DEFAULT: 'hsl(var(--card))',
+          DEFAULT: 'hsl(var(--background) / 0.9)',
           foreground: 'hsl(var(--sidebar-foreground))',
           primary: 'hsl(var(--sidebar-primary))',
           'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
