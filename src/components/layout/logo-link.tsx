e27effcf936b01cff5af70e83d9afe59825b342e
Desire Koua <desire0369@gmail.com>
2025-10-03 00:49:52 +0000

    fait en sorte que tout es bouton qui doit rédiriger vers la page d'acceu

--- src/components/layout/logo-link.tsx
+++ src/components/layout/logo-link.tsx
@@ -1,35 +1,13 @@
 
 'use client';
 
-import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import Cookies from 'js-cookie';
-import { useRouter } from 'next/navigation';
-import { cn } from '@/lib/utils';
 
 export default function LogoLink() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const router = useRouter();
-
-    useEffect(() => {
-        const token = Cookies.get('auth_token');
-        if (token === 'true') {
-            setIsAuthenticated(true);
-        }
-    }, []);
-
-    const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-        if (isAuthenticated) {
-            e.preventDefault();
-            router.push('/dashboard');
-        }
-    };
-
     return (
         <Link 
-            href="/login" 
-            onClick={handleClick}
+            href="/" 
             className="flex items-center gap-2 md:gap-3"
         >
             <Image src="https://i.postimg.cc/jS25XGKL/Capture-d-cran-2025-09-03-191656-4-removebg-preview.png" width={32} height={32} alt="Statut Predict Logo" className="h-8 w-8 md:h-10 md:w-10" />
