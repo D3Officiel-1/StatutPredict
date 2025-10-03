3186dcb05d1abc2c7205588bf563101eb57f0d01
Desire Koua <desire0369@gmail.com>
2025-10-03 16:42:56 +0000

    /ui/checkbox /media-library /ui/input
    crée les fichier ci dessus dans l

--- vercel/path0/src/components/dashboard/notification-form.tsx
+++ vercel/path0/src/components/dashboard/notification-form.tsx
@@ -19,10 +19,10 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 import { useToast } from '@/hooks/use-toast';
 import { Sparkles, Send, Upload, GalleryHorizontal } from 'lucide-react';
 import CustomLoader from '@/components/ui/custom-loader';
-import { Checkbox } from '../ui/checkbox';
+import { Checkbox } from '@/components/ui/checkbox';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import MediaLibrary from './media-library';
-import { Input } from '../ui/input';
+import { Input } from '@/components/ui/input';
 import Image from 'next/image';
 
 const formSchema = z.object({
@@ -224,7 +224,7 @@ export default function NotificationForm() {
       console.error('Error sending notification:', error);
       toast({
         title: 'Erreur',
-        description: 'Impossible d\'envoyer ou de sauvegarder la notification.',
+        description: 'Impossible d\\'envoyer ou de sauvegarder la notification.',
         variant: 'destructive',
       });
     } finally {
